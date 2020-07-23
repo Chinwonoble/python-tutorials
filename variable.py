d663b74b252866f0e3653d8b7b940cc0d8377f8d
chinwonoble <azchinwo@gmail.com>
2020-07-23 05:52:02 -0700

    Initial Project Version

---
+++ variable.py
@@ -0,0 +1,25 @@
+x = 1 
+y = 2.5
+name = 'John'
+is_cool = True
+fav_colors = ['white', 'grey', 'black']
+# assignment give 5 examples of variable names and assign values to tehm of different types
+
+a = 4
+g = 5.5
+name = 'noble'
+is__sunny = False
+footballers = ['ronaldo', 'messi', 'pulisic']
+
+'''
+this is
+a python class
+where we will be learning how to code
+'''
+
+print(y)
+print(is_cool)
+print(x)
+print(fav_colors)
+
+
